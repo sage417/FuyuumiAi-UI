fc853ee480074b6f4a1bdbf8a49e53cf2201fc69
sage417 <sage417@foxmail.com>
2016-09-01 23:08:25 +0800

    service

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,21 +1,40 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
+import { HeroService } from './hero.service';
+import {Hero} from './hero'
 
 @Component({
     selector: 'my-hero-detail',
-    template:`
-            <div *ngIf="hero">
-                <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
+    template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
+      <div>
+        <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
-            </div>
-            </div>
-`,
+      </div>
+    </div>
+  `
 })
-export class HeroDetailComponent {
-    @Input()
-    hero: Hero;
+export class HeroDetailComponent implements OnInit {
+    @Input() hero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.route.params.forEach((params: Params) => {
+            let id = +params['id'];
+            this.heroService.getHero(id)
+                .then(hero => this.hero = hero);
+        });
+    }
+
+
 }
